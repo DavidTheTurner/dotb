9e6b5b2435148670bb8bc9cee9801920e33cb929
David Turner <david.turner@valentpartners.com>
2023-12-26 17:48:17 -0600

    added unit test for app container

---
+++ test/specs/appContainer.unit.ts
@@ -0,0 +1,28 @@
+import { browser } from "@wdio/globals";
+
+describe("AppContainer", () => {
+  afterEach(async () => {
+    await browser.reloadSession();
+  });
+
+  it("should minimize the window", async () => {
+    const minimizeButton = await browser.$("#minimize");
+    await minimizeButton.click();
+    expect(await browser.$("#app-container")).toHaveAttr("minimized");
+  });
+
+  it("should maximize the window", async () => {
+    const maximizeButton = await browser.$("#maximize");
+    await maximizeButton.click();
+    expect(await browser.$("#app-container")).toHaveAttr("maximized");
+    await maximizeButton.click();
+    expect(await browser.$("#app-container")).not.toHaveAttr("maximized");
+  });
+
+  it("should close the window", async () => {
+    const closeButton = await browser.$("#close");
+    const appContainer = await browser.$("#app-container");
+    await closeButton.click();
+    expect(appContainer).not.toBeExisting();
+  });
+});
